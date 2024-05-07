1944ba68e22621976eda3f2969867837e438f1c7
Rienk Kuipers <rienk@kuipers.org>
2024-05-07 22:42:42 +0200

    Cleaner Code and added of custom-greeter to xgreeters dir

--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
+exit 1
 sudo apt update
 sudo apt upgrade -y
 mkdir /install/
 cp ./* /install/
+chmod +x /install/custom-greeter.py
 echo ""
 echo ""
 echo ""
 echo ""
-echo "Apt updates done"
+echo "Apt updates done and moving of files done."
 echo "(1/5)"
 echo ""
 echo ""
@@ -27,10 +29,12 @@ echo ""
 echo ""
 sudo apt install xserver-xorg --no-install-recommends --no-install-suggests -y
 sudo apt install xfce4 -y
-sudo apt install xfce4-terminal -y
-sudo apt install neofetch -y
-sudo apt install lightdm --no-install-recommends --no-install-suggests -y
-sudo apt install lightdm-gtk-greeter --no-install-recommends --no-install-suggests -y
+sudo apt install xfce4-terminal -y # Terminal
+sudo apt install neofetch -y # Sys stat util
+sudo apt install lightdm --no-install-recommends --no-install-suggests -y # Login Manager
+sudo apt install lightdm-gtk-greeter --no-install-recommends --no-install-suggests -y # Technically no req but good to lock
+sudo apt install python3 # Install Python
+sudo apt install liblightdm-gobject-dev # Python Dependency to run the custom greeter
 sudo systemctl enable --now lightdm
 echo ""
 echo ""
@@ -41,3 +45,6 @@ echo "(3/5)"
 echo ""
 echo ""
 echo ""
+
+sudo sed -i 's/#greeter-session=.*$/greeter-session=custom-greeter/' /etc/lightdm/lightdm.conf # Set to use custom greeter
+sudo cp ./custom-greeter.desktop /usr/share/xgreeters/
